3975b40c17071be27e364dc1ac93c12fe4aecd26
Matheusfelipea <matheusfelipe_801@hotmail.com>
2022-04-04 20:38:41 -0300

    Alterações

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Meu servidor");
+const config = require ("config");
+const express = require("express");
+const bodryparser = require("body-parser");
+console.log("Abrindo meu servidor ");
+
+let port = config.get("server.port");
+
+//console.log("porta:", porta);
+
+const app = express();
+
+app.listen(port, function() {
+    console.log (`Servidor rodando na porta ${port}`)
+});
